a6a4d6e5a5472c93d2ef22c9a503468c7e1becb3
wangming98160 <92302883+wangming98160@users.noreply.github.com>
2022-01-26 13:48:32 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -2,6 +2,6 @@ const app = require("express")(),
   jsyaml = require("js-yaml");
 
 app.get("load", function(req, res) {
-  let data = jsyaml.load(req.params.data);
+  let data = jsyaml.safeLoad(req.params.data);
   // ...
 });
